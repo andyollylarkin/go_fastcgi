5422f140105e0193de55b635339e712125f0a43c
Denis Davydov <follenlast2@gmail.com>
2022-09-24 20:57:26 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_fastcgi
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// import "net"
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+)
+
+
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+        w.Write([]byte("This is a FastCGI example server.\n"))
+}
+
+
+func main(){
+	fmt.Println("Starting server...")
+	l, _ := net.Listen("tcp", "127.0.0.1:9000")
+	b := new(FastCGIServer)
+	fcgi.Serve(l, b)
+}
